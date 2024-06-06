b8826c132854bfe9b31a7b41637132a977d446f2
Ajay Ambule <ajayambule2003@gmail.com>
2024-06-06 10:49:59 +0530

    Index.js file updated

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+//
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
