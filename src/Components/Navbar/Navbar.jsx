5b7a539acf943b1ed8748a22bab99d769d44131b
Ajay Ambule <ajayambule2003@gmail.com>
2024-06-13 10:46:32 +0530

    Updated Files...!

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,8 +7,8 @@ import logo from './../../Assets/logo.png';
 export default function Navbar() {
     const { colorMode, toggleColorMode } = useColorMode();
     const { isOpen, onOpen, onClose } = useDisclosure();
-
-    return (
+ 
+    return ( 
         <>
             <Box id='navbar' bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
                 <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
